afaf6a7e25b7d882586172d03e039371d38506f7
王涛 <wangtao_yzj@beyondcent.com>
2021-08-17 11:47:09 +0800

    add quickstart-k8s

---
+++ deploy/cluster/add-edge.sh
@@ -0,0 +1,48 @@
+#!/bin/bash
+set -e
+
+CONTAINER_MANAGER=${CONTAINER_MANAGER:-docker}
+CONTAINER_NAME=deploy
+IMAGE_NAME=deploy:0.1.0
+EXTRA_VARS="container_manager=${CONTAINER_MANAGER} ${EXTRA_VARS}"
+HOST_VARS=$@
+
+
+function info() {
+    echo '[INFO] ' "$@"
+}
+function warn() {
+    echo '[WARN] ' "$@" >&2
+}
+function fatal() {
+    echo '[ERROR] ' "$@" >&2
+    exit 1
+}
+
+function validate_container_manager() {
+    case $CONTAINER_MANAGER in
+        docker)
+            :
+            ;;
+        *)
+            fatal "unsupported CONTAINER_MANAGER: ${CONTAINER_MANAGER}"
+            ;;
+    esac
+}
+
+
+function docker_add_edge() {
+    docker restart ${CONTAINER_NAME} > /dev/null
+    docker exec ${CONTAINER_NAME} bash -c "( DEBUG=False /usr/bin/python /opt/deploy.py --job-chain edge-deploy.yaml --skip-deployed-job false --limit '!master*' --extra-vars ${EXTRA_VARS} ${HOST_VARS} )"
+}
+
+function add_edge() {
+    if [ "$CONTAINER_MANAGER" = "docker" ];then
+        docker_add_edge
+    fi
+}
+
+
+validate_container_manager
+
+add_edge
