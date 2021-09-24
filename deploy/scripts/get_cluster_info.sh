29f272caf31ed4c5e0a5d3d0160fee494f08a97d
wanglet <15233265+wanglet@users.noreply.github.com>
2021-09-24 09:48:56 +0800

    Feature/update helm chart (#121)
    * helm-chart: update chart
    
    * installer: update get_cluster_info.sh
    
    * installer: update get_cluster_info.sh
    
    * helm-chart: update edgeLabels
    
    Co-authored-by: 王涛 <wangtao_yzj@beyondcent.com>

--- deploy/scripts/get_cluster_info.sh
+++ deploy/scripts/get_cluster_info.sh
@@ -5,10 +5,7 @@ echo ""
 
 while read line
 do
-    if [[ "$line" =~ \"--cluster-cidr=.* ]];
-    then
-        cluster_cidr=`awk -F '["=]' '{print $3}' <<< $line`
-    elif [[ "$line" =~ \"--cluster-name=.* ]];
+    if [[ "$line" =~ \"--cluster-name=.* ]];
     then
         cluster_name=`awk -F '["=]' '{print $3}' <<< $line`
     elif [[ "$line" =~ \"--service-cluster-ip-range=.* ]];
@@ -18,8 +15,8 @@ do
 done <<< "`kubectl cluster-info dump | awk '(/cluster-cidr/ || /cluster-name/ || /service-cluster-ip-range/) && !a[$0]++{print}'`"
 
 
-cluster_dns=$(kubectl get cm nodelocaldns -n kube-system -o jsonpath="{.data.Corefile}" | awk '/bind/ && !a[$0]++{print $2}')
+cluster_dns=$(kubectl get cm nodelocaldns -n kube-system -o jsonpath="{.data.Corefile}" 2> /dev/null | awk '/bind/ && !a[$0]++{print $2}')
 
-echo "edgecore clusterDNS   : $cluster_dns"
-echo "edgecore clusterDomain: $cluster_name"
-echo "helm connectorSubnets : $cluster_cidr,$service_cluster_ip_rang"
+echo "clusterDNS               : $cluster_dns"
+echo "clusterDomain            : $cluster_name"
+echo "service-cluster-ip-range : $service_cluster_ip_rang"
