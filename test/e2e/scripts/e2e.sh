e33013b18fea31518b8fa13b680eeddde9a9d961
genghaotao <genghaotao@beyondcent.com>
2021-09-28 14:40:08 +0800

    docs: add integration guide with openyurt

--- test/e2e/scripts/e2e.sh
+++ test/e2e/scripts/e2e.sh
@@ -4,7 +4,8 @@ kubectl delete community all-edge-nodes
 kubectl delete ns fabedge-e2e-test
 
 # edge-labels:
-# kubedge: node-role.kubernetes.io/connector=
+# kubedge: node-role.kubernetes.io/connector
 # superedge: superedge.io/edge-node=enable
+# openyurt: openyurt.io/is-edge-worker=true
 
 ./fabedge-e2e.test -wait-timeout 600 -ping-timeout 300 -curl-timeout 300 --edge-labels "node-role.kubernetes.io/connector"
