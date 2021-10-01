537f257177ccadea58c6a8047a6f331daa5874c5
haotao geng <genghaotao@beyondcent.com>
2021-10-01 10:17:58 +0800

    Update env_prepare.sh

--- pkg/agent/env_prepare.sh
+++ pkg/agent/env_prepare.sh
@@ -6,9 +6,10 @@ set -x
 find /etc/cni/net.d/ -type f -not -name fabedge.conf -exec rm {} \;
 cp -f /usr/local/bin/bridge /usr/local/bin/host-local /usr/local/bin/loopback /opt/cni/bin
 
-# clear the CNI Flannel's residual data
+# cleanup the CNI Flannel's residual data
 ip link delete cni0
 ip link delete flannel.1
 ip route | grep "flannel" |  while read dst via gw others; do ip route delete $dst via $gw; done
+iptables -t nat -F POSTROUTING
 
 exit 0
