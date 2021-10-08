ff2d6050e88d132c6a67214ee9059a2ebb53e53f
genghaotao <genghaotao@beyondcent.com>
2021-10-08 09:51:11 +0800

    agent: update nat outgoing rules

--- pkg/agent/env_prepare.sh
+++ pkg/agent/env_prepare.sh
@@ -2,14 +2,18 @@
 
 set -x
 
-# install CNI plug-ins
+# install CNI plugins
 find /etc/cni/net.d/ -type f -not -name fabedge.conf -exec rm {} \;
 cp -f /usr/local/bin/bridge /usr/local/bin/host-local /usr/local/bin/loopback /opt/cni/bin
 
-# cleanup the CNI Flannel's residual data
+# cleanup flannel stuff
 ip link delete cni0
 ip link delete flannel.1
 ip route | grep "flannel" |  while read dst via gw others; do ip route delete $dst via $gw; done
 iptables -t nat -F POSTROUTING
+
+# cleanup xfrm stuff
+ip xfrm policy flush
+ip xfrm state flush
 
 exit 0
