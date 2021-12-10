84eb5df0b7334a96136a1bb11f9e2635ce623014
王涛 <wangtao_yzj@beyondcent.com>
2021-12-10 15:48:04 +0800

    checker: update checker

--- deploy/scripts/checker.sh
+++ deploy/scripts/checker.sh
@@ -13,26 +13,6 @@ function echoBlue() {
    echo -e "\033[34m$@ \033[0m"
 }
 
-function echoBanner() {
-  name=$1
-  color=$2
-  nameLength=$(echo $name | wc -L)
-  shellWidth=`stty size|awk '{print $2}'`
-  outWord='-'
-  outWordSize=$(expr $shellWidth - $nameLength)
-  outWordLeftSize=$(expr $outWordSize / 2)
-  outWordRightSize=$(expr $shellWidth - $nameLength - $outWordLeftSize)
-
-  banner=$(printf %${outWordLeftSize}s |tr " " "${outWord}"; echo -n $name; printf %${outWordRightSize}s |tr " " "${outWord}";echo)
-  if [[ $color == green ]];then
-    echoGreen $banner
-  elif [[ $color == red ]]; then
-    echoRed $banner
-  else
-    echo $banner
-  fi
-}
-
 function printColorOutput() {
   command=$1
   keyworld=${2:-"********************"}
@@ -51,13 +31,9 @@ function printColorOutput() {
   echo
 }
 
-
 function checkNodes () {
-  echoBlue "$ kubectl get nodes -o wide"
-
-  nodesInfo=$(kubectl get nodes -o wide)
-  exitCode=$?
-  failed="false"
+  command="kubectl get nodes -o wide"
+  echoBlue "$ $command"
 
   while read line; do
     read name status roles age version <<< $line
@@ -70,9 +46,8 @@ function checkNodes () {
       echo "$line"
     else
       echoRed "$line"
-      failed="true"
     fi
-  done <<< "$nodesInfo"
+  done <<< "$($command)"
 }
 
 function checkPods () {
@@ -92,39 +67,34 @@ function checkPods () {
 }
 
 function checkKubeSystemPods () {
-  echoBlue "$ kubectl get pods -n kube-system -o wide"
-  podsInfo=$(kubectl get pods -n kube-system -o wide)
-  exitCode=$?
+  command="kubectl get pods -n kube-system -o wide"
+  echoBlue "$ $command"
+  podsInfo=$($command)
   checkPods kube-system
 }
 
 function checkFabedgePods () {
-  echoBlue "$ kubectl get pods -n $FABEDGE_NAMESPACE -o wide"
-  podsInfo=$(kubectl get pods -n $FABEDGE_NAMESPACE -o wide)
-  exitCode=$?
+  command="kubectl get pods -n $FABEDGE_NAMESPACE -o wide"
+  echoBlue "$ $command"
+  podsInfo=$($command)
   checkPods $FABEDGE_NAMESPACE
 }
 
-function printStrongswanConnsAndSas () {
+function printFabedgeDetails () {
   podsInfo=$(kubectl get pods -n $FABEDGE_NAMESPACE)
-  exitCode=$?
-  if [ $exitCode != 0 ]; then
-    return 1
-  fi
+
   while read name ready status restarts age; do
-    if [[ $name =~ ^fabedge ]];then
-      printColorOutput "kubectl -n $FABEDGE_NAMESPACE describe pods $name"
+    printColorOutput "kubectl -n $FABEDGE_NAMESPACE describe pods $name"
 
-      if [[ $name =~ ^fabedge-agent|^fabedge-connector ]];then
-        printColorOutput "kubectl exec -n $FABEDGE_NAMESPACE $name -c strongswan -- swanctl --list-conns"
-        printColorOutput "kubectl exec -n $FABEDGE_NAMESPACE $name -c strongswan -- swanctl --list-sa"
-      fi
-
-      for containerName in $(kubectl -n $FABEDGE_NAMESPACE get pods $name -o jsonpath='{.spec.initContainers[*].name} {.spec.containers[*].name}');
-      do
-        printColorOutput "kubectl logs --tail 50 -n $FABEDGE_NAMESPACE $name -c $containerName"
-      done
+    if [[ $name =~ agent|connector ]];then
+      printColorOutput "kubectl exec -n $FABEDGE_NAMESPACE $name -c strongswan -- swanctl --list-conns"
+      printColorOutput "kubectl exec -n $FABEDGE_NAMESPACE $name -c strongswan -- swanctl --list-sa"
     fi
+
+    for containerName in $(kubectl -n $FABEDGE_NAMESPACE get pods $name -o jsonpath='{.spec.initContainers[*].name} {.spec.containers[*].name}');
+    do
+      printColorOutput "kubectl logs --tail 50 -n $FABEDGE_NAMESPACE $name -c $containerName"
+    done
   done <<< "$podsInfo"
 }
 
@@ -132,6 +102,9 @@ function printIPRouteInfo() {
   printColorOutput "ip l"
   printColorOutput "ip r"
   printColorOutput "ip r s t 220"
+}
+
+function printXFRMInfo() {
   printColorOutput "ip x p"
   printColorOutput "ip x s"
 }
@@ -157,15 +130,17 @@ case ${1} in
     checkNodes
     checkKubeSystemPods
     checkFabedgePods
-    printStrongswanConnsAndSas
+    printFabedgeDetails
     printIPRouteInfo
     ;;
   connector)
     printIPRouteInfo
+    printXFRMInfo
     printIptablesInfo
     ;;
   edge)
     printIPRouteInfo
+    printXFRMInfo
     printIptablesInfo
     ;;
   *)
