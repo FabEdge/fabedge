0993333cbe23819946f5328d1ec96d756e717bf5
wangtao <wangtao_yzj@beyondcent.com>
2022-03-09 16:04:15 +0800

    utils no_proxy: replace gitee to github

--- build/utils/no_proxy.sh
+++ build/utils/no_proxy.sh
@@ -6,6 +6,7 @@ set -ex
 
 for app in agent connector operator strongswan
 do
+   sed -i 's/gitee/github/g' build/$app/Dockerfile
    sed -i 's/GOPROXY/NOGOPROXY/g' build/$app/Dockerfile
    sed -i 's/dl-cdn.alpinelinux.org/nothing-to-do/g' build/$app/Dockerfile
 done
