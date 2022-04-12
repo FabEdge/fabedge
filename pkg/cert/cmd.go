3e27f750745b34cb7a03bb0b0e6f1eae570b9678
yanjianbo <yanjianbo@beyondcent.com>
2022-04-12 16:17:42 +0800

    cert: add view command
    Signed-off-by: yanjianbo <yanjianbo@beyondcent.com>

--- pkg/cert/cmd.go
+++ pkg/cert/cmd.go
@@ -17,11 +17,15 @@ package cert
 import (
 	"bytes"
 	"context"
+	"crypto/rsa"
 	"crypto/x509"
 	"encoding/pem"
 	"flag"
 	"fmt"
+	"net"
+	"net/url"
 	"os"
+	"strings"
 
 	"github.com/spf13/cobra"
 	corev1 "k8s.io/api/core/v1"
@@ -226,6 +230,32 @@ fabedge-cert verify --secret edge --ca-secret=fabedge-ca --namespace=fabedge
 		},
 	}
 
+	viewCertCmd := &cobra.Command{
+		Use:   "view secretName",
+		Short: "View the certificate of a TLS secret",
+		Args:  cobra.ExactArgs(1),
+		Run: func(cmd *cobra.Command, args []string) {
+			key := client.ObjectKey{Name: args[0], Namespace: globalOptions.Namespace}
+			cert := getCertificateFromSecret(key)
+			fmt.Printf("Version: %d\n", cert.Version)
+			fmt.Printf("Subject: %s\n", cert.Subject)
+			fmt.Printf("Issuer: %s\n", cert.Issuer)
+			fmt.Printf("IsCA: %t\n", cert.IsCA)
+			fmt.Printf("Signature Algorithm: %s\n", cert.SignatureAlgorithm)
+			fmt.Printf("Publickey Algorithm: %s\n", cert.PublicKeyAlgorithm)
+			fmt.Printf("Validity: \n")
+			fmt.Printf("      Not Before: %s\n", cert.NotBefore)
+			fmt.Printf("      Not After: %s\n", cert.NotAfter)
+			fmt.Printf("Key length: %d\n", cert.PublicKey.(*rsa.PublicKey).Size()*8)
+			fmt.Printf("Key Usage: %s\n", formatKeyUsage(cert.KeyUsage))
+			fmt.Printf("Ext Key Usage: %s\n", formatExtUsages(cert.ExtKeyUsage))
+			fmt.Printf("DNS Names: %s\n", strings.Join(cert.DNSNames, " "))
+			fmt.Printf("IP Addresses: %s\n", formatIPs(cert.IPAddresses))
+			fmt.Printf("Email Addresses: %s\n", strings.Join(cert.EmailAddresses, " "))
+			fmt.Printf("URIs: %s\n", formatURIs(cert.URIs))
+		},
+	}
+
 	versionCmd := &cobra.Command{
 		Use:   "version",
 		Short: "Display version information",
@@ -249,6 +279,7 @@ fabedge-cert verify --secret edge --ca-secret=fabedge-ca --namespace=fabedge
 		genCmd,
 		saveCaCmd,
 		verifyCmd,
+		viewCertCmd,
 		versionCmd,
 	)
 
@@ -373,6 +404,27 @@ func getCertAndKeyFromSecret(key client.ObjectKey, certName, keyName string) (ce
 	return decodePEM(secret.Data[certName]), decodePEM(secret.Data[keyName])
 }
 
+func getCertificateFromSecret(key client.ObjectKey) *x509.Certificate {
+	var secret corev1.Secret
+	err := createKubeClient().Get(context.TODO(), key, &secret)
+	if err != nil {
+		exit("failed to get secret: %s", err)
+	}
+
+	pemBytes := secret.Data[corev1.TLSCertKey]
+	if len(pemBytes) == 0 {
+		pemBytes = secret.Data[secretutil.KeyCACert]
+	}
+
+	cert, err := x509.ParseCertificate(decodePEM(pemBytes))
+	if err != nil {
+		exit("failed to decode certificate: %s", err)
+	}
+
+	return cert
+
+}
+
 func decodePEM(data []byte) []byte {
 	block, _ := pem.Decode(data)
 	if block == nil {
@@ -392,4 +444,111 @@ func doValidations(validateFns ...func() error) func(cmd *cobra.Command, args []
 
 		return nil
 	}
+}
+
+func formatKeyUsage(keyUsage x509.KeyUsage) string {
+	var usages []string
+	isUsed := func(expected x509.KeyUsage) bool {
+		return keyUsage&expected == expected
+	}
+
+	if isUsed(x509.KeyUsageDigitalSignature) {
+		usages = append(usages, "DigitalSignature")
+	}
+
+	if isUsed(x509.KeyUsageContentCommitment) {
+		usages = append(usages, "ContentCommitment")
+	}
+
+	if isUsed(x509.KeyUsageKeyEncipherment) {
+		usages = append(usages, "KeyEncipherment")
+	}
+
+	if isUsed(x509.KeyUsageDataEncipherment) {
+		usages = append(usages, "DataEncipherment")
+	}
+
+	if isUsed(x509.KeyUsageKeyAgreement) {
+		usages = append(usages, "KeyAgreement")
+	}
+
+	if isUsed(x509.KeyUsageCertSign) {
+		usages = append(usages, "CertSign")
+	}
+
+	if isUsed(x509.KeyUsageCRLSign) {
+		usages = append(usages, "CRLSign")
+	}
+
+	if isUsed(x509.KeyUsageEncipherOnly) {
+		usages = append(usages, "EncipherOnly")
+	}
+
+	if isUsed(x509.KeyUsageDecipherOnly) {
+		usages = append(usages, "DecipherOnly")
+	}
+
+	return strings.Join(usages, " ")
+}
+
+func formatExtUsages(extKeyUsages []x509.ExtKeyUsage) string {
+	var usages []string
+
+	for _, eku := range extKeyUsages {
+		t := ""
+		switch eku {
+		case x509.ExtKeyUsageAny:
+			t = "Any"
+		case x509.ExtKeyUsageServerAuth:
+			t = "ServerAuth"
+		case x509.ExtKeyUsageClientAuth:
+			t = "ClientAuth"
+		case x509.ExtKeyUsageCodeSigning:
+			t = "CodeSigning"
+		case x509.ExtKeyUsageEmailProtection:
+			t = "EmailProtection"
+		case x509.ExtKeyUsageIPSECEndSystem:
+			t = "IPSECEndSystem"
+		case x509.ExtKeyUsageIPSECTunnel:
+			t = "IPSECTunnel"
+		case x509.ExtKeyUsageIPSECUser:
+			t = "IPSECUser"
+		case x509.ExtKeyUsageTimeStamping:
+			t = "TimeStamping"
+		case x509.ExtKeyUsageOCSPSigning:
+			t = "OCSPSigning"
+		case x509.ExtKeyUsageMicrosoftServerGatedCrypto:
+			t = "MicrosoftServerGatedCrypto"
+		case x509.ExtKeyUsageNetscapeServerGatedCrypto:
+			t = "NetscapeServerGatedCrypto"
+		case x509.ExtKeyUsageMicrosoftCommercialCodeSigning:
+			t = "MicrosoftCommercialCodeSigning"
+		case x509.ExtKeyUsageMicrosoftKernelCodeSigning:
+			t = "MicrosoftKernelCodeSigning"
+		}
+
+		if t != "" {
+			usages = append(usages, t)
+		}
+	}
+
+	return strings.Join(usages, " ")
+}
+
+func formatIPs(ips []net.IP) string {
+	var values []string
+	for _, ip := range ips {
+		values = append(values, ip.String())
+	}
+
+	return strings.Join(values, " ")
+}
+
+func formatURIs(urls []*url.URL) string {
+	var values []string
+	for _, url := range urls {
+		values = append(values, url.String())
+	}
+
+	return strings.Join(values, " ")
 }
